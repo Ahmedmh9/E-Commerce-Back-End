360634155b416105334089341b7478012937ac4b
Mohamed Ahmed <axmedmohamed7@gmail.com>
2021-10-25 17:23:42 -0500

    product association

--- models/index.js
+++ models/index.js
@@ -10,8 +10,12 @@ Product.belongsTo(Category, {
   onDelete: 'CASCADE'
 });
 // Categories have many Products
+Category.hasMany(Product, {
+  foreignKey: 'category_id'
+})
 
 // Products belongToMany Tags (through ProductTag)
+
 
 // Tags belongToMany Products (through ProductTag)
 
