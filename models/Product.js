ef6bc776ac8bc77458a5826ef3c36470249f0c5c
Mohamed Ahmed <axmedmohamed7@gmail.com>
2021-10-25 16:54:47 -0500

    product model

--- models/Product.js
+++ models/Product.js
@@ -1,23 +1,97 @@
 // import important parts of sequelize library
-const { Model, DataTypes } = require('sequelize');
+const { Model, DataTypes } = require("sequelize");
 // import our database connection from config.js
-const sequelize = require('../config/connection');
+const sequelize = require("../config/connection");
 
 // Initialize Product model (table) by extending off Sequelize's Model class
 class Product extends Model {}
 
 // set up fields and rules for Product model
 Product.init(
+  // define columns
   {
-    // define columns
+    id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement: true,
+    },
+    product_name: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    price: {
+      type: DataTypes.DECIMAL,
+      allowNull: false,
+      validate: {
+        isDecimal: true,
+      },
+    },
+    stock: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      defaultValue: 10,
+      validate: {
+        isNumeric: true,
+      },
+    },
+    category_id: {
+      type: DataTypes.INTEGER,
+      //references the Category model's id
+      references: {
+        model: "category",
+        key: "id",
+      },
+    },
   },
   {
     sequelize,
     timestamps: false,
     freezeTableName: true,
     underscored: true,
-    modelName: 'product',
+    modelName: "product",
   }
 );
 
 module.exports = Product;
+/*Product
+1.id
+
+Integer.
+
+Doesn't allow null values.
+
+Set as primary key.
+
+Uses auto increment.
+
+
+
+
+
+2.product_name
+
+String.
+
+Doesn't allow null values.
+
+
+3.price
+
+Decimal.
+
+Doesn't allow null values.
+
+Validates that the value is a decimal.
+
+
+
+4.stock
+
+Integer.
+
+Doesn't allow null values.
+
+Set a default value of 10.
+
+Validates that the value is numeric.*/
