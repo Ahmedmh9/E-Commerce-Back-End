b0f43d9fd0b58596e327c635727a427b19366341
Mohamed Ahmed <axmedmohamed7@gmail.com>
2021-10-25 17:08:25 -0500

    defined ProductTag

--- models/ProductTag.js
+++ models/ProductTag.js
@@ -7,6 +7,29 @@ class ProductTag extends Model {}
 ProductTag.init(
   {
     // define columns
+    id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement: true,
+    },
+    product_id: {
+      type: DataTypes.INTEGER,
+      //references the product model's id
+      references: {
+        model: "Product",
+        key: "id",
+      },
+    },
+    tag_id: {
+      type: DataTypes.INTEGER,
+      //references the tag model's id
+      references: {
+        model: "Tag",
+        key: "id",
+      },
+    }
+    
   },
   {
     sequelize,
@@ -18,3 +41,27 @@ ProductTag.init(
 );
 
 module.exports = ProductTag;
+
+/* ProductTag
+
+1.id
+
+Integer.
+
+Doesn't allow null values.
+
+Set as primary key.
+
+Uses auto increment.
+
+2.product_id
+
+Integer.
+
+References the Product model's id.
+
+3.tag_id
+
+Integer.
+
+References the Tag model's id. */
