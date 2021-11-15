6a7c07a4ebf273586202c9a90219751db41ad6a7
Mohamed Ahmed <axmedmohamed7@gmail.com>
2021-11-14 22:59:55 -0600

    made router to find by id,and create a category

--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -18,20 +18,38 @@ router.get("/", async (req, res) => {
   } catch (err) {
     res.status(500).json(err);
   }
-  // be sure to include its associated Products
+  
 });
 
 router.get("/:id", async (req, res) => {
   // find one category by its `id` value
-  // be sure to include its associated Products
+  try {
+    const categoryData = await Category.findByPk(req.params.id, {
+      include: [{
+        model: Product,
+        required: true
+      }]
+    });
+    res.status(200).json(categoryData);
+  } catch (err) {
+    res.status(500).json(err);
+  }
 });
 
 router.post("/", async (req, res) => {
   // create a new category
+  try {
+    const newCategory = await Category.create(req.body);
+    res.status(200).json(newCategory);
+  } catch (err) {
+    res.status(400).json(err);
+  }
 });
 
 router.put("/:id", async (req, res) => {
   // update a category by its `id` value
+  
+ 
 });
 
 router.delete("/:id", async (req, res) => {
